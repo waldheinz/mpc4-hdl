2f8757b24a325b33c6d0a4ac7faa1567bf19e92a
Matthias Treydte <waldheinz@gmail.com>
2012-03-25 11:13:34 +0200

    GDC: Worked proper host interface.

--- U82720_CTRL.vhd
+++ U82720_CTRL.vhd
@@ -3,16 +3,18 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity U82720_CTRL is
-    Port ( D : in  STD_LOGIC_VECTOR (8 downto 0);
-           D_RDY : in  STD_LOGIC;
-           D_FUL : in  STD_LOGIC;
-           RD : out  STD_LOGIC;
-           WR : in  STD_LOGIC);
+	Port (
+		CLK	: in STD_LOGIC;
+		RESET	: in STD_LOGIC; -- async reset
+		D : in  STD_LOGIC_VECTOR (8 downto 0);
+		D_RDY : in  STD_LOGIC;
+		D_FUL : in  STD_LOGIC;
+		RD : out  STD_LOGIC;
+		WR : in  STD_LOGIC);
 end U82720_CTRL;
 
 architecture RTL of U82720_CTRL is
-
+	
 begin
-
-
+	
 end RTL;
--- U82720_UC.vhd
+++ U82720_UC.vhd
@@ -4,82 +4,87 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 --
--- Microcontroller Interface of U82720
+-- Microcontroller Interface of U82720 (the bidirectional FIFO)
 --
 
 entity U82720_UC is
 	Port (
-		DB 	: inout	STD_LOGIC_VECTOR (7 downto 0);
-		
-		-- read:  0 -> status register, 1 -> FIFO
-		-- write: 0 -> FIFO parameter,  1 -> FIFO command
+		--
+		-- Host Interface
+		--
+		DB_H 		: inout	STD_LOGIC_VECTOR (7 downto 0); -- data bus to host
 		A0 		: in  	STD_LOGIC;
-      RD_n 		: in  	STD_LOGIC;
-      WR_n		: in  	STD_LOGIC;
-		LPD		: in		STD_LOGIC;  -- light pen detect
+      RDH_n 	: in  	STD_LOGIC;
+      WRH_n		: in  	STD_LOGIC;
+		
+		--
+		-- Internal Interface
+		--
+		DB_I		: inout	STD_LOGIC_VECTOR(7 downto 0); -- internal data bus
+		RDI_n		: in		STD_LOGIC;
+		WRI_n		: in		STD_LOGIC;
+		DB_DIR	: in		STD_LOGIC; -- 0 -> from host to gdc, 1 -> from gdc to host
+		LPD		: in		STD_LOGIC; -- light pen detect
 		HSYNC		: in		STD_LOGIC;
 		VSYNC		: in 		STD_LOGIC;
-		DMA_A		: in 		STD_LOGIC;  -- DMA active
+		DMA_A		: in 		STD_LOGIC; -- DMA active
 		PAINT		: in 		STD_LOGIC; -- paint in progress
-		RESET_n	: out	STD_LOGIC);
+		EMPTY		: out		STD_LOGIC; -- FIFO empty
+		RESET_n	: out		STD_LOGIC);
 end U82720_UC;
 
 architecture RTL of U82720_UC is
 	-- FIFO
 	subtype FIFO_WORD is std_logic_vector(8 downto 0);
 	type FIFO_TABLE is array(0 to 15) of FIFO_WORD;
-	signal fifo_rd	: unsigned(3 downto 0);
-	signal fifo_wr : unsigned(3 downto 0);
+	signal fifo_h : unsigned(3 downto 0); -- fifo position for host interface
+	signal fifo_i : unsigned(3 downto 0); -- fifo position for internal interface
 	signal fifo : FIFO_TABLE;
 	
 	-- 7: light pen detect, 6: hblank, vsync, 5: vsnyc, 4: dma exec, 3: drawing in progress, 2: FIFO empty, 1: FIFO full, 0: data ready
 	signal status_reg : std_logic_vector (7 downto 0);
-	
-	signal data_out 	: std_logic_vector (7 downto 0);
-	-- if a reset was requested with the last write cycle
-	signal reset : std_logic;
+	signal data_out 	: std_logic_vector (7 downto 0); -- data output buffer
+	signal reset : std_logic; -- if a reset was requested with the last write cycle
 	
 begin
-	DB <= data_out when (RD_n = '0') else (others => 'Z');
+	DB_H <= data_out when (RDH_n = '0') else (others => 'Z');
 	
 	status_reg(7) <= LPD;
 	status_reg(6) <= HSYNC;
 	status_reg(5) <= VSYNC;
 	status_reg(4) <= DMA_A;
 	status_reg(3) <= PAINT;
-	status_reg(2) <= '1' when (fifo_rd = fifo_wr) else '0'; -- FIFO empty
-	status_reg(1) <= '1' when (fifo_rd = (fifo_wr + 1)) else '0'; -- FIFO full (keep one slot empty)
+	status_reg(2) <= '1' when (fifo_h = fifo_i) else '0'; -- FIFO empty
+	status_reg(1) <= '1' when (fifo_h = (fifo_i + 1)) else '0'; -- FIFO full (keep one slot empty)
 	RESET_n <= reset;
 	
-	proc_write: process (DB, A0, WR_n, fifo_wr)
+	proc_host: process (DB_H, A0, RDH_n, WRH_n, fifo_h, status_reg)
 	begin
-		if (rising_edge(WR_n)) then
+		if (rising_edge(WRH_n)) then
 			reset <= '1';
 			
-			if ((A0 = '1') and (DB = "00000000")) then
+			if ((A0 = '1') and (DB_H = "00000000")) then
 				-- RESET
 				reset <= '0';
-				fifo_wr <= (others => '0');
+				fifo_h <= (others => '0');
 			else
-				-- write FIFO command
-				fifo(to_integer(fifo_wr)) <= A0 & DB;
-				fifo_wr <= fifo_wr + 1;
+				-- FIFO write
+				fifo(to_integer(fifo_h)) <= A0 & DB_H;
+				fifo_h <= fifo_h + 1;
+			end if;
+		elsif (falling_edge(RDH_n)) then			
+			if (A0 = '0') then
+				data_out <= status_reg;
+			else
+				data_out <= fifo(to_integer(fifo_h))(7 downto 0);
+				fifo_h <= fifo_h + 1;
 			end if;
 		end if;
 	end process;
 	
-	proc_read: process (A0, RD_n, fifo_rd, status_reg, reset)
+	proc_int : process(reset)
 	begin
-		if (reset = '0') then
-			fifo_rd <= (others => '0');
-		elsif (falling_edge(RD_n)) then			
-			if (A0 = '0') then
-				data_out <= status_reg;
-			else
-				data_out <= fifo(to_integer(fifo_rd))(7 downto 0);
-				fifo_rd <= fifo_rd + 1;
-			end if;
-		end if;
+		
 	end process;
 	
 end RTL;
