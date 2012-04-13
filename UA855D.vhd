020ec0f3ef4e068bfcf6bcbaf19bf38ac28f88ba
Matthias Treydte <waldheinz@gmail.com>
2012-04-13 12:44:30 +0200

    PIO: Added module stub.

---
+++ UA855D.vhd
@@ -0,0 +1,45 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+entity UA855D is
+   Port (
+      -- Clock
+      C        : in     STD_LOGIC;
+      
+      -- CPU Data Bus
+      D        : inout  STD_LOGIC_VECTOR (7 downto 0);
+      
+      -- PIO Control
+      B_A_SEL  : in     STD_LOGIC;
+      C_D_SEL  : in     STD_LOGIC;
+      CS_n     : in     STD_LOGIC;
+      M1_n     : in     STD_LOGIC;
+      IORQ_n   : in     STD_LOGIC;
+      RD_n     : in     STD_LOGIC;
+      
+      -- Interrupt Control
+      IEI      : in     STD_LOGIC;
+      IEO      : out    STD_LOGIC;
+      INT_n    : out    STD_LOGIC;
+      
+      -- Port A I/O
+      A        : inout  STD_LOGIC_VECTOR (7 downto 0);
+      ARDY     : out    STD_LOGIC;
+      ASTB_n   : in     STD_LOGIC;
+      
+      -- Port B I/O
+      B        : inout  STD_LOGIC_VECTOR (7 downto 0);
+      BRDY     : out    STD_LOGIC;
+      BSTB_n   : in     STD_LOGIC);
+end UA855D;
+
+architecture RTL of UA855D is
+   
+begin
+   
+end RTL;
