81a826f8e34aac023b00ee73e2e634dafe7da984
Matthias Treydte <waldheinz@gmail.com>
2012-04-14 11:58:57 +0200

    pio: Added some port status handling.

--- UA855D.vhd
+++ UA855D.vhd
@@ -37,10 +37,20 @@ end UA855D;
 
 architecture RTL of UA855D is
    
+   type PORT_REG_TABLE is array(1 downto 0) of std_logic_vector(7 downto 0);
+   
+   type PORT_CTRL_STATE_TYPE is (CS_WAIT_CMD, CS_WAIT_IOSEL);
+   type PORT_CTRL_STATE_TBL is array(1 downto 0) of PORT_CTRL_STATE_TYPE;
+   signal port_ctrl_state : PORT_CTRL_STATE_TBL;
+   
+   -- Port Modes
    type PORT_MODE_TYPE is (M_OUTP, M_INPU, M_BIDI, M_CTRL);
    type PORT_MODE_TABLE is array(1 downto 0) of PORT_MODE_TYPE;
-   
    signal port_mode : PORT_MODE_TABLE;
+   
+   -- Port specific Registers
+   signal port_out      : PORT_REG_TABLE; -- Port Output Registers
+   signal port_io_sel   : PORT_REG_TABLE; -- Port I/O Select
    
    signal reset_n : std_logic; -- internal async reset signal
    signal port_select : integer range 0 to 1; -- 0 is port A, 1 is B
@@ -48,23 +58,37 @@ architecture RTL of UA855D is
 begin
    D <= (others => 'Z');
    
-   reset_n <= '0' when (M1_n = '0' and RD_n = '1' and IORQ_n = '1') else '1';
+   reset_n <= '0' when (M1_n = '0' and RD_n = '1' and IORQ_n = '1' and CS_n = '0') else '1';
    port_select <= 0 when B_A_SEL = '0' else 1;
    
    proc_ctrl : process (B_A_SEL, C_D_SEL, CLK, D, RD_n, port_select)
    begin
-      if (rising_edge(CLK) and RD_n = '1' and C_D_SEL = '1') then
-         case D(3 downto 0) is
-            when "1111" => -- set mode
-               case D(7 downto 6) is
-                  when "00"   => port_mode(port_select) <= M_OUTP;
-                  when "01"   => port_mode(port_select) <= M_INPU;
-                  when "10"   => port_mode(port_select) <= M_BIDI;
-                  when others => port_mode(port_select) <= M_CTRL;
+      if (reset_n = '0') then
+         for i in 0 to 1 loop
+            port_ctrl_state(i) <= CS_WAIT_CMD;
+         end loop;
+      elsif (rising_edge(CLK) and CS_n = '0' and RD_n = '1' and C_D_SEL = '1') then
+         case port_ctrl_state(port_select) is
+            when CS_WAIT_CMD =>
+               case D(3 downto 0) is
+                  when "1111" => -- set mode
+                     case D(7 downto 6) is
+                        when "00"   => port_mode(port_select) <= M_OUTP;
+                        when "01"   => port_mode(port_select) <= M_INPU;
+                        when "10"   => port_mode(port_select) <= M_BIDI;
+                        when others =>
+                           port_mode(port_select) <= M_CTRL;
+                           port_ctrl_state(port_select) <= CS_WAIT_IOSEL;
+                     end case;
+               
+                  when others => null;
                end case;
+             
+            when CS_WAIT_IOSEL =>
+               port_io_sel(port_select) <= D;
+               port_ctrl_state(port_select) <= CS_WAIT_CMD;
                
             when others => null;
-               
          end case;
       end if;
    end process;
